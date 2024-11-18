5d919809e534d9e661f697cdb0a325980de563f3
Tymur <tymurkulivar@gmail.com>
2024-11-18 23:19:34 +0100

    Move main to another package for better testing

--- src/main/java/prueba/Main.java
+++ src/main/java/prueba/Main.java
@@ -1,7 +1,8 @@
-package com.github.ironbit;
+package prueba;
 
-import com.github.ironbit.files.CodeVertFile;
-import com.github.ironbit.files.JsonFile;
+import com.github.ironbit.CodeVertFile;
+import com.github.ironbit.FileConverter;
+import com.github.ironbit.JsonFile;
 
 import java.io.File;
 
@@ -10,9 +11,10 @@ public class Main {
         //Creamos el objeto al ejecutar el evento de Drag and Drop
         File userFile = new File("/home/tymur/Documents/Api_Try/GUESTS.JSON");
 
-        CodeVertFile file = new JsonFile(userFile);
-
         FileConverter converter = new FileConverter();
+
+        JsonFile file = new JsonFile(userFile);
+
         converter.convert(file, "XML");
     }
 }
